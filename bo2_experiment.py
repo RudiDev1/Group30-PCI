138c5b1560578e985117d63c5ba71e38bcbf265d
Rudolf Rusek <rudolf.rusek@gmail.com>
2026-06-11 22:40:28 +0200

    Changes  to bo2_experiment.py
    Changes to SwarmAgent and simulation.
    
    Swarm agent now has four states, exploration, dissamination, voting, and wait. It explores at the start or if undecided after voting phase. Whether bots add a frame to it's decision is dependent to quality (in this case probability) of the site it is on Dissamination uses cross-validation where if the count of bots agreeing with it is below zero, then the bot becomes undecided, otherwise stays the same, Voting takes in votes from each agent, changes whether bot waits for others, or starts exploration again.
    
    Simulation now includes one site with image of a beach 500x500. Second site is the open area.

--- bo2_experiment.py
+++ bo2_experiment.py
@@ -5,41 +5,78 @@ from vi.config import Config
 
 # randomness setup
 random.seed(a=42)
+# global vars to return
+votes: dict = {"A": 0, "B":0}
 @dataclass
 class BestOf2Config(Config): ...
 class SwarmAgent(Agent[BestOf2Config]):
     def __init__(self, images, simulation, pos = None, move = None):
         super().__init__(images, simulation, pos, move)
-        self.counter: dict = {"A": 0, "B":0} #Amount of nests of given type encountered during the exploretion
+        self.counter: dict = {"A": 0, "B":0} #Amount of time spent in each nest during the exploretion
         self.commitment: str = "uncommited"
-        self.current_phase = "exploration"
+        self.states: dict = {"exploration": 1000, "dissamination": 0}
+        self.state = "exploration"
+        self.ticks = 0
+        self.prng = random.Random()
+        self.prng.seed(42)
     def update(self):
-        if self.current_phase == "exploration":
-            self.exploration_phase()
-    def after_update(self):
-        pass
-    def exploration_phase(self):
-        pass
-class NestAgent(Agent[BestOf2Config]):
-    def __init__(self, images, simulation, pos = None, move = None, nest_id = None):
-        super().__init__(images, simulation, move)
-        self.pos.x = random.randint(0, 750)
-        self.pos.y = random.randint(0, 750)
-        self.nest_id 
-    def on_spawn(self):
-        self.freeze_movement()
-        if 100 <= self.id <= 119:
-            self.nest_id = "A"
-        elif 120 <= self.id:
-            self.nest_id = "B"
+        if self.state == "exploration" and self.states["exploration"] >= self.ticks:
+            if self.on_site():
+                if self.prng.random() < 0.7:
+                    self.counter["A"] += 1
+            else:
+                if self.prng.random() < 0.3:
+                    self.counter["B"] += 1
+            self.ticks += 1
+        elif self.state == "exploration" and self.states["exploration"] < self.ticks:
+            self.decision()
+            self.state = "dissamination"
+        elif self.state == "dissamination":
+            self.freeze_movement()
+            count = 0
+            for agent, _ in self.in_proximity_accuracy():
+                if agent.commitment == self.commitment:
+                    count += 1
+                elif agent.commitment != self.commitment:
+                    count -= 1
+                else: #change the commitment to first neighbour option if uncommited
+                    self.commitment = agent.commitment 
+                    break
+            if count <= 0:
+                self.commitment = "uncommited"
+                self.state = "voting"
+            else:
+                self.state = "voting"
+        elif self.state == "voting":
+            self.voting()
+        elif self.state == "wait":
+            pass
+    def decision(self):
+        if self.counter["A"] > self.counter["B"]:
+            self.commitment = "A"
+        elif self.counter["B"] > self.counter["A"]:
+            self.commitment = "B"
+        #the ones with equal number of encounters (by chance) stay uncommited
+    def voting(self):
+        if self.commitment == "A":
+            votes["A"] += 1
+            self.change_image(1)
+            self.state = "wait"
+        elif self.commitment == "B":
+            votes["B"] += 1
+            self.change_image(2)
+            self.state = "wait"
+        else:
+            self.state = "exploration"
+            self.continue_movement()
         
 class ExperimentWindow(Window): ...
 
 
 (
-    Simulation(BestOf2Config(image_rotation=False, movement_speed=1, radius=10, window=ExperimentWindow()))
-    .batch_spawn_agents(100, SwarmAgent, images=["images/triangle.png"])
-    .batch_spawn_agents(20, NestAgent, images=["images/red.png"])
-    .batch_spawn_agents(10, NestAgent, images=["images/green.png"])
+    Simulation(BestOf2Config(image_rotation=False, movement_speed=1, radius=15, window=ExperimentWindow(), duration = 8000, visualise_chunks = True))
+    .spawn_site(image_path="images/images.png", x = 750 //2, y = 750 //2)
+    .batch_spawn_agents(100, SwarmAgent, images=["images/triangle.png", "images/green.png", "images/red.png"])   
     .run()
 )
+print(votes)
