5ccdbef9b769929f5ebb8b73cc8c133fd7bc3892
Zach Massarella <88383060+ZachMassarella@users.noreply.github.com>
2026-06-12 13:12:39 +0200

    Updated Actual Experiment - Error Persists with Trail

--- bo2_experiment.py
+++ bo2_experiment.py
@@ -71,24 +71,29 @@ class SwarmAgent(Agent[BestOf2Config]):
         else:
             self.state = "exploration"
             self.continue_movement()
+
+class CustomSimulation(Simulation):
+    def before_update(self) -> None:
         
-class ExperimentWindow(Window):
-    def stop_condition(self) -> bool:
-        agents = self._sim._agents
-        total_agents = 100 #hardcoded - remember to change if we change num agents
+        agents = self._agents
+        total_agents = 100 
         
         count_a: int = sum(1 for agent in agents if agent.commitment == 'A')
         count_b: int = sum(1 for agent in agents if agent.commitment == 'B')
 
-        threshold_agents = 0.8 * total_agents #again threshold is hardcoded to 80% consensus - remember to change
+        threshold_agents = 0.8 * total_agents 
+        
         if count_a >= threshold_agents or count_b >= threshold_agents:
-            print('Terminated due to Agent Threshold Being Met')
-            return True
-        return False
+            final_time: float = time.perf_counter()
+            total_time: float = final_time - start_time
+            print(f"Terminated due to Threshold Being Met A: {count_a}, B: {count_b}")
+            print(f"Time - {total_time}")
+            self.stop()
 
+class ExperimentWindow(Window): ...
 
 (
-    Simulation(BestOf2Config(image_rotation=False, movement_speed=1, radius=15, window=ExperimentWindow(), duration = 8000, visualise_chunks = True))
+    CustomSimulation(BestOf2Config(image_rotation=False, movement_speed=1, radius=15, window=ExperimentWindow(), duration = 80000, visualise_chunks = True))
     .spawn_site(image_path="images/images.png", x = 750 //2, y = 750 //2)
     .batch_spawn_agents(100, SwarmAgent, images=["images/triangle.png", "images/green.png", "images/red.png"])   
     .run()
