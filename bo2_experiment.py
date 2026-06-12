eb7671ee010ad15b50b9a58917809616e51687ff
Rudolf Rusek <rudolf.rusek@gmail.com>
2026-06-12 15:31:14 +0200

    Update bo2_experiment.py
    Changed the threshold to be all agents agree, counter is now reset after agents enters exploration again

--- bo2_experiment.py
+++ bo2_experiment.py
@@ -29,6 +29,7 @@ class SwarmAgent(Agent[BestOf2Config]):
         elif self.state == "exploration" and self.states["exploration"] < self.ticks:
             self.decision()
             self.state = "dissamination"
+            self.ticks = 0
         elif self.state == "dissamination":
             #self.freeze_movement()
             count = 0
@@ -60,6 +61,7 @@ class SwarmAgent(Agent[BestOf2Config]):
             x = self.prng.random()
             if x <= 0.5:
                 self.state = "exploration"
+                self.counter = {"A": 0, "B": 0}
             elif x > 0.5:
                 self.state = "dissamination"
         elif self.commitment == "B":
@@ -67,6 +69,7 @@ class SwarmAgent(Agent[BestOf2Config]):
             x = self.prng.random()
             if x <= 0.5:
                 self.state = "exploration"
+                self.counter = {"A": 0, "B": 0}
             elif x > 0.5:
                 self.state = "dissamination"
         else:
@@ -88,9 +91,9 @@ class CustomSimulation(Simulation):
         count_a: int = sum(1 for agent in agents if agent.commitment == 'A')
         count_b: int = sum(1 for agent in agents if agent.commitment == 'B')
 
-        threshold_agents = 0.8* total_agents 
+        threshold_agents = total_agents 
 
-        if current % 2000 == 0:
+        if current % 1002 == 0:
             print(f"Threshold of A: {count_a}, B: {count_b} at {current}")
         
         if count_a >= threshold_agents or count_b >= threshold_agents:
@@ -105,7 +108,7 @@ class CustomSimulation(Simulation):
 class ExperimentWindow(Window): ...
 
 (
-    CustomSimulation(BestOf2Config(image_rotation=False, movement_speed=1, radius=75, window=ExperimentWindow(), duration = 80000, visualise_chunks = True))
+    CustomSimulation(BestOf2Config(image_rotation=False, movement_speed=1, radius=50, window=ExperimentWindow(), duration = 80000, visualise_chunks = True))
     .spawn_site(image_path="images/images.png", x = 750 //2, y = 750 //2)
     .batch_spawn_agents(100, SwarmAgent, images=["images/triangle.png", "images/green.png", "images/red.png"])   
     .run()
